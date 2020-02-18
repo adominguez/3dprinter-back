4a5e131cbd91a606f2bdcd2bd55b6e71d9d1210e
ADominguez <dmartin.alberto@gmail.com>
2020-02-18 19:18:48 +0100

    chore(index): update hour

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ appCategories.getCategories(app);
 
 // schedule tasks to be run on the server
 // 'seg min hora * * *'
-schedule.cron('00 16 18 * * *');
+schedule.cron('00 00 17 * * *');
 
 // Server port listening
 app.listen(PORT, function () {

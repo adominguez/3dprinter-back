46bfd180d529ca13197efc6f29667f27cb4c5a33
ADominguez <dmartin.alberto@gmail.com>
2020-02-27 21:00:38 +0100

    chore(email): check with console.log the server

--- utils/email.js
+++ utils/email.js
@@ -6,16 +6,21 @@ const fetch = require('node-fetch');
  */
 exports.sendEmail = (data) => {
   const { subject, message, fromEmail, toEmail, type } = data;
+
+  console.log(secret.urlSendEmail)
+
   if (subject && message) {
     const url = `${secret.urlSendEmail}?subject=${subject}&message=${message}${fromEmail && '&fromEmail=' + fromEmail || ''}${toEmail && '&toEmail=' + toEmail || ''}${type && '&type=' + type || ''}`;
     fetch(url)
       .then(function (response) {
+        console.log('ha enviado el correo')
         return response;
       })
-  } else {
-    return {
-      errorCode: 400,
-      errorMessage: 'Los parámetros subject y message son obligatorios'
-    }
+    } else {
+      console.log('ha habido un error en el envío de correo')
+      return {
+        errorCode: 400,
+        errorMessage: 'Los parámetros subject y message son obligatorios'
+      }
   }
 }

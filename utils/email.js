5e917ba51b36d62be1a35157e5c895342bddbd7a
ADominguez <dmartin.alberto@gmail.com>
2020-02-28 20:11:30 +0100

    feat(email): add template and change email

--- utils/email.js
+++ utils/email.js
@@ -4,11 +4,34 @@ const helper = require('sendgrid').mail;
  * To commit this file it´s necessary uncomment in gitignore file
  */
 exports.sendEmail = (data) => {
-  const { subject, message, fromEmail = '3dmakernow@gmail.com', toEmail = '3dmakernow@gmail.com', type } = data;
+  const { subject, message, fromEmail = 'noreply@3dmakernow.com', toEmail = '3dmakernow@gmail.com', type } = data;
 
   const from_email = new helper.Email(fromEmail);
   const to_email = new helper.Email(toEmail);
-  var content = new helper.Content('text/plain', subject);
+  var content = new helper.Content('text/html', `
+  <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <title>Correo enviado desde 3DMakerNow</title>
+    </head>
+    <body>
+      <table width="100%" border="0" cellspacing="3" cellpadding="3">
+        <tr>
+          <td><img src="https://3dmakernow.com/wp-content/uploads/2018/03/cropped-logotipo-eslogan-1.png" /></td>
+        </tr>
+        <tr>
+          <td>
+            <p>
+              ${message}
+            </p>
+          </td>
+        </tr>
+      </table>
+    </body>
+  </html>`);
+
+  console.log('content', content);
+  console.log('subject', subject);
 
   if (subject && message) {
     var mail = new helper.Mail(from_email, subject, to_email, content);

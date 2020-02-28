2ad62b7d330826bd503822082fde14e50b78e111
ADominguez <dmartin.alberto@gmail.com>
2020-02-28 22:28:49 +0100

    fix(index): change hour of schedule

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ appCategories.getCategories(app);
 
 // schedule tasks to be run on the server
 // 'seg min hora * * *'
-schedule.cron('00 29 22 * * *');
+schedule.cron('00 30 21 * * *');
 
 // Server port listening
 app.listen(PORT, function () {

351df973c79aa27e22db911b9a7f58f85ef7b69f
ADominguez <dmartin.alberto@gmail.com>
2020-02-28 19:48:25 +0100

    fix(email): fix typographic error in var

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ exports.sendEmail = (data) => {
   if (subject && message) {
     var mail = new helper.Mail(from_email, subject, to_email, content);
 
-    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var sg = require('sendgrid')(process.env.SENGRID_API_KEY);
     var request = sg.emptyRequest({
       method: 'POST',
       path: '/v3/mail/send',

ea209c2b723df019ce07eb6933c57d3f4793b6f8
ADominguez <dmartin.alberto@gmail.com>
2020-04-05 22:40:26 +0200

    feat(schedule, getPrices): add new countries and products like object & refactor getPrices

--- schedule.js
+++ schedule.js
@@ -2,6 +2,79 @@ const cron = require("node-cron");
 const firebase = require('./app');
 const getPrices = require('./utils/getPrices');
 
+
+
+const printersdb = firebase.db.ref('3d-printers');
+const printers = printersdb.once('value');
+printers.then(snapshot => {
+  const printersData = snapshot.val();
+  printersId = Object.keys(printersData);
+  let spanishProducts = [];
+  let mxProducts = [];
+  let usaProducts = [];
+  let aliexpressProducts = [];
+  let gearbestProducts = [];
+  printersElements = Object.values(printersData);
+  printersElements.forEach(element => {
+    const {affiliateAmazonInfo: {ES, MX, US}, affiliateAliexpress, affiliateGearbestInfo} = element;
+    /**
+     * Añadimos los productos de ES
+     */
+    if(ES) {
+      if(ES.asin) {
+        spanishProducts.push(ES);
+      } else {
+
+        Object.values(ES).forEach(item => spanishProducts.push(item))
+      }
+    }
+    /**
+     * Añadimos los productos de MX
+     */
+    if(MX) {
+      if(MX.asin) {
+        mxProducts.push(MX);
+      } else {
+        Object.values(MX).forEach(item => mxProducts.push(item))
+      }
+    }
+    /**
+     * Añadimos los productos de US
+     */
+    if(US) {
+      if(US.asin) {
+        usaProducts.push(US);
+      } else {
+        Object.values(US).forEach(item => usaProducts.push(item))
+      }
+    }
+    /**
+     * Añadimos los productos de aliexpress
+     */
+    if(affiliateAliexpress) {
+      if(affiliateAliexpress.asin) {
+        aliexpressProducts.push(affiliateAliexpress);
+      } else {
+        Object.values(affiliateAliexpress).forEach(item => aliexpressProducts.push(item))
+      }
+    }
+    /**
+     * Añadimos los productos de Gearbest
+     */
+    if(affiliateGearbestInfo) {
+      if(affiliateGearbestInfo.asin) {
+        gearbestProducts.push(affiliateGearbestInfo);
+      } else {
+        Object.values(affiliateGearbestInfo).forEach(item => gearbestProducts.push(item))
+      }
+    }
+  });
+  getPrices.getAmazonProductPrice({spanishProducts, mxProducts, usaProducts}, '3d-printers');
+  // getPrices.getAliexpressProductPrice(aliexpressProducts, id, '3d-printers');
+  // getPrices.getGearbestProductPrice(gearbestProducts, id, '3d-printers');
+});
+
+
 exports.cronPrinters = (scheduleTime) => {
   cron.schedule(scheduleTime, async () => {
     try {

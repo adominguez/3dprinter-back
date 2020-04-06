a5297ac4c1469d91246e2cb1a7eb853f755e23bb
ADominguez <dmartin.alberto@gmail.com>
2020-04-06 19:49:20 +0200

    feat(schedule): refactor schedule and add formatProducts function to reutilize function and good practices

--- schedule.js
+++ schedule.js
@@ -3,79 +3,73 @@ const firebase = require('./app');
 const getPrices = require('./utils/getPrices');
 
 
-
-const printersdb = firebase.db.ref('3d-printers');
-const printers = printersdb.once('value');
-printers.then(snapshot => {
-  const printersData = snapshot.val();
-  printersId = Object.keys(printersData);
-  let spanishProducts = [];
-  let mxProducts = [];
-  let usaProducts = [];
-  let aliexpressProducts = [];
-  let gearbestProducts = [];
-  let printersElements = [];
-  Object.entries(printersData).forEach(([id, value]) => {
-    printersElements.push({id, ...value})
+const formatProducts = (products) => {
+  return new Promise(resolve => {
+    let spanishProducts = [];
+    let mxProducts = [];
+    let usaProducts = [];
+    let aliexpressProducts = [];
+    let gearbestProducts = [];
+    let productsElements = [];
+    Object.entries(products).forEach(([id, value]) => {
+      productsElements.push({ id, ...value })
+    });
+    productsElements.forEach(element => {
+      const { affiliateAmazonInfo: { ES, MX, US }, affiliateAliexpress, affiliateGearbestInfo, id, name: productName } = element;
+      /**
+       * Añadimos los productos de ES
+       */
+      if (ES) {
+        if (ES.asin) {
+          spanishProducts.push({ ...ES, id, productName, subproduct: false });
+        } else {
+          Object.values(ES).forEach(item => spanishProducts.push({ ...item, id, productName, subproduct: true }))
+        }
+      }
+      /**
+       * Añadimos los productos de MX
+       */
+      if (MX) {
+        if (MX.asin) {
+          mxProducts.push({ ...MX, id, productName, subproduct: false });
+        } else {
+          Object.values(MX).forEach(item => mxProducts.push({ ...item, id, productName, subproduct: true }))
+        }
+      }
+      /**
+       * Añadimos los productos de US
+       */
+      if (US) {
+        if (US.asin) {
+          usaProducts.push({ ...US, id, productName, subproduct: false });
+        } else {
+          Object.values(US).forEach(item => usaProducts.push({ ...item, id, productName, subproduct: true }))
+        }
+      }
+      /**
+       * Añadimos los productos de aliexpress
+       */
+      if (affiliateAliexpress) {
+        if (affiliateAliexpress.asin) {
+          aliexpressProducts.push({ ...affiliateAliexpress, id, productName, subproduct: false });
+        } else {
+          Object.values(affiliateAliexpress).forEach(item => aliexpressProducts.push({ ...item, id, productName, subproduct: true }))
+        }
+      }
+      /**
+       * Añadimos los productos de Gearbest
+       */
+      if (affiliateGearbestInfo) {
+        if (affiliateGearbestInfo.asin) {
+          gearbestProducts.push({ ...affiliateGearbestInfo, id, productName, subproduct: false });
+        } else {
+          Object.values(affiliateGearbestInfo).forEach(item => gearbestProducts.push({ ...item, id, productName, subproduct: true }))
+        }
+      }
+      resolve({ spanishProducts, mxProducts, usaProducts, aliexpressProducts, gearbestProducts });
+    })
   });
-  printersElements.forEach(element => {
-    const {affiliateAmazonInfo: {ES, MX, US}, affiliateAliexpress, affiliateGearbestInfo, id, name: productName} = element;
-    /**
-     * Añadimos los productos de ES
-     */
-    if(ES) {
-      if(ES.asin) {
-        spanishProducts.push({...ES, id, productName, subproduct: false});
-      } else {
-        Object.values(ES).forEach(item => spanishProducts.push({...item, id, productName, subproduct: true}))
-      }
-    }
-    /**
-     * Añadimos los productos de MX
-     */
-    if(MX) {
-      if(MX.asin) {
-        mxProducts.push({...MX, id, productName, subproduct: false});
-      } else {
-        Object.values(MX).forEach(item => mxProducts.push({...item, id, productName, subproduct: true}))
-      }
-    }
-    /**
-     * Añadimos los productos de US
-     */
-    if(US) {
-      if(US.asin) {
-        usaProducts.push({...US, id, productName, subproduct: false});
-      } else {
-        Object.values(US).forEach(item => usaProducts.push({...item, id, productName, subproduct: true}))
-      }
-    }
-    /**
-     * Añadimos los productos de aliexpress
-     */
-    if(affiliateAliexpress) {
-      if(affiliateAliexpress.asin) {
-        aliexpressProducts.push({...affiliateAliexpress, id, productName, subproduct: false});
-      } else {
-        Object.values(affiliateAliexpress).forEach(item => aliexpressProducts.push({...item, id, productName, subproduct: true}))
-      }
-    }
-    /**
-     * Añadimos los productos de Gearbest
-     */
-    if(affiliateGearbestInfo) {
-      if(affiliateGearbestInfo.asin) {
-        gearbestProducts.push({...affiliateGearbestInfo, id, productName, subproduct: false});
-      } else {
-        Object.values(affiliateGearbestInfo).forEach(item => gearbestProducts.push({...item, id, productName, subproduct: true}))
-      }
-    }
-  });
-  getPrices.getAmazonProductPrice({spanishProducts, mxProducts, usaProducts}, '3d-printers');
-  getPrices.getAliexpressProductPrice(aliexpressProducts, '3d-printers');
-  getPrices.getGearbestProductPrice(gearbestProducts, '3d-printers');
-});
-
+}
 
 exports.cronPrinters = (scheduleTime) => {
   cron.schedule(scheduleTime, async () => {
@@ -86,12 +80,16 @@ exports.cronPrinters = (scheduleTime) => {
       const printersdb = await firebase.db.ref('3d-printers');
       const printers = await printersdb.once('value');
       const printersData = printers.val();
-      printersId = Object.keys(printersData);
-      printersId.forEach(item => {
-        getPrices.getAmazonProductPrice(printersData[item], item, '3d-printers');
-        getPrices.getAliexpressProductPrice(printersData[item], item, '3d-printers');
-        getPrices.getGearbestProductPrice(printersData[item], item, '3d-printers');
-      });
+      const {
+        spanishProducts,
+        mxProducts,
+        usaProducts,
+        aliexpressProducts,
+        gearbestProducts
+      } = await formatProducts(printersData);
+      getPrices.getAmazonProductPrice({ spanishProducts, mxProducts, usaProducts }, '3d-printers');
+      getPrices.getAliexpressProductPrice(aliexpressProducts, '3d-printers');
+      getPrices.getGearbestProductPrice(gearbestProducts, '3d-printers');
     } catch (error) {
       console.log(error);
     }
@@ -107,12 +105,16 @@ exports.cronMaterials = (scheduleTime) => {
       const materialsdb = await firebase.db.ref('materials');
       const materials = await materialsdb.once('value');
       const materialsData = materials.val();
-      materialsId = Object.keys(materialsData);
-      materialsId.forEach(item => {
-        getPrices.getAmazonProductPrice(materialsData[item], item, 'materials');
-        getPrices.getAliexpressProductPrice(materialsData[item], item, 'materials');
-        getPrices.getGearbestProductPrice(materialsData[item], item, 'materials');
-      });
+      const {
+        spanishProducts,
+        mxProducts,
+        usaProducts,
+        aliexpressProducts,
+        gearbestProducts
+      } = await formatProducts(materialsData);
+        getPrices.getAmazonProductPrice({ spanishProducts, mxProducts, usaProducts }, 'materials');
+        getPrices.getAliexpressProductPrice(aliexpressProducts, 'materials');
+        getPrices.getGearbestProductPrice(gearbestProducts, 'materials');
     } catch (error) {
       console.log(error);
     }
